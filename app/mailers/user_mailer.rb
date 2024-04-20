6d5e4e217eb66dcfe1525d2ae51baf092ac128a4
gervais <fseetp@gmail>
2024-04-21 00:18:53 +0200

    change name

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,11 @@
 class UserMailer < Devise::Mailer
-  default from: 'administrateur@culture-test.com'
+  default from: 'administrateur@musical-knowledge.com'/
 
   
   def welcome_email(user)
     @user = user
     @url  = ['http://example.com/login']
-    mail(to: @user.email, subject: ['Bienvenue sur culture-test'])
+    mail(to: @user.email, subject: ['Bienvenue sur musical-knowledge'])
   end
 
   def reset_password_instructions(record, token, opts={})
