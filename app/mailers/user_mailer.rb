f715bc7c1cdc22860388347ddc4ccfe8e0af688f
gervais <fseetp@gmail>
2024-04-21 16:01:46 +0200

    chnamemailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,11 @@
 class UserMailer < Devise::Mailer
-  default from: 'administrateur@musical-knowledge.com'/
+  default from: 'administrateur@culture-sound.com'/
 
   
   def welcome_email(user)
     @user = user
     @url  = ['http://example.com/login']
-    mail(to: @user.email, subject: ['Bienvenue sur musical-knowledge'])
+    mail(to: @user.email, subject: ['Bienvenue sur culture-sound'])
   end
 
   def reset_password_instructions(record, token, opts={})
