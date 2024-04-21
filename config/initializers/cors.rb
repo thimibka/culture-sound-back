fb5928ab325c4cb3f91f9c0b80e5fe49adafd8a2
gervais <fseetp@gmail>
2024-04-21 15:15:32 +0200

    config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -16,7 +16,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 
   allow do
-    origins 'https://https://musical-knowledge.vercel.app/'
+    origins 'https://culture-sound.vercel.app'
 
     resource '*',
             headers: :any,
